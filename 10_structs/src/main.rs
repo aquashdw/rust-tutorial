db70fbe3484aae40e7b73b1be06433923b8c4d75
aquashdw <aquashdw@gmail.com>
2026-09-21 09:29:45 +0900

    struct update syntax

--- 10_structs/src/main.rs
+++ 10_structs/src/main.rs
@@ -33,6 +33,24 @@ fn main() {
     let user2 = build_user(String::from("brad@gmail.com"), String::from("brad"));
     let user2_email = user2.email;
     println!("{user2_email}");
+
+    // say we need to make user2 from user1
+    /*
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("alex@naver.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+    */
+    // we can use struct update syntax to unpack fields from another struct instance
+    let user2 = User {
+        email: String::from("alex@naver.com"),
+        ..user1  // no trailing comma allowed
+    };
+    // the struct update syntax moves user1's ownership, meaning we can't use user1 afterwards
+    // which also goes for the above, where we manually use user1's fields for user2
+    // let user1_email = user1.email;  // Value used after being moved
 }
 
 fn build_user(email: String, username: String) -> User {

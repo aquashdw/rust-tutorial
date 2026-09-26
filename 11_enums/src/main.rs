7691cc64061cb206045cfecbd4f45a48759a796a
aquashdw <aquashdw@gmail.com>
2026-09-26 12:08:47 +0900

    enums: using enums with values

--- 11_enums/src/main.rs
+++ 11_enums/src/main.rs
@@ -3,23 +3,33 @@ enum IpAddrKind {
     V6,
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// instead of enum inside a struct, we can add value inside an enum
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    // let home = IpAddr {
+    //     kind: IpAddrKind::V4,
+    //     address: String::from("127.0.0.1"),
+    // };
+    //
+    // let loopback = IpAddr {
+    //     kind: IpAddrKind::V6,
+    //     address: String::from("::1"),
+    // };
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    // using enums with values
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr::V6(String::from("::1"));
 }
 
